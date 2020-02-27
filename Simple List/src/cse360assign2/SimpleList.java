8da4aadae2233f3ca1a61357ff94257e56fdad6a
alarc <alarc@10.157.226.122>
2020-02-27 15:17:06 -0700

    Implemented dynamic sizing of Simple List and updated toString method.

--- Simple List/src/cse360assign2/SimpleList.java
+++ Simple List/src/cse360assign2/SimpleList.java
@@ -1,29 +1,28 @@
 /**
  * @author Daniel Alarcon
  * Class ID: 139
- * Assignment #: 1
+ * Assignment #: 2
  * 
  * Description:
- * 	SimpleList is an array of ten integers. Integers can be added with the add
- * method and integers can be removed with the remove method. The count method
- * returns the number of added integers in the SimpleList.
+ * 	SimpleList, by default, is an array of ten integers. Integers can be added
+ * with the add method and integers can be removed with the remove method. The
+ * count method returns the number of added integers in the SimpleList.
  * 
  * 	When integers are added, the contents of the SimpleList are shifted to the
  * right, to the next higher index value, and the new integer is inserted into
- * index 0. The count is then incremented. When the SimpleList is full and a 
- * new integer is being added, the last integer essentially "falls off" and is
- * lost. The count remains the same because the number of integers in the 
- * SimpleList has not changed.
+ * index 0. The count is then incremented. If the Simple List is full when a
+ * new integer is being added, the size of Simple List is increased by 50%
+ * before inserting a new integer. The count is then incremented.
  * 
  * 	When integers are removed, the search method finds the integer in the
  * SimpleList and returns the index of that integer. Otherwise, the search
  * method returns a -1. Using that index, the contents of the SimpleList are
  * shifted to the left, to the next lower index value, starting from that index
- * from the search method. The integer is essentially overwritten. Finally, the
- * last integer of the SimpleList is cleared to 0 and the count is decremented.
- * If the queried integer is not found in the SimpleList, which the search
- * method returns a -1, the SimpleList is not modified and the remove method
- * exits.
+ * from the search method. The integer is essentially overwritten. After the
+ * integer has been removed, if there is more than 25% of unused space in the
+ * Simple List, then the size of the Simple List is decreased by 25%. If the
+ * queried integer is not found in the SimpleList, which the search method
+ * returns a -1, the SimpleList is not modified and the remove method exits.
  * 
  *	The toString method returns the SimpleList as a String. The String returned
  * is the integers in the SimpleList separated by a space. However, the last
@@ -34,7 +33,11 @@ package cse360assign2;
 
 public class SimpleList {
 	
-	final int MAXSIZE = 10;
+	final int DEFAULT_SIZE = 10;
+	final int SMALLEST_SIZE = 1;
+	final float INCREASE_SIZE = 1.5f;
+	final float DECREASE_SIZE = 0.75f;
+	final float EMPTY_THRESHOLD = 0.25f;
 	
 	private int[] list;
 	private int count;
@@ -43,27 +46,42 @@ public class SimpleList {
 	 * Class constructor for SimpleList.
 	 */
 	public SimpleList() {
-		list = new int[MAXSIZE];
+		list = new int[DEFAULT_SIZE];
 		count = 0;
 	}
 	
 	/**
 	 * Add an element to index 0 by shifting all elements to the right starting
-	 * from the highest index.
+	 * from the highest index. If the list is full, then the list is copied
+	 * over to a new list that is 50% bigger in size.
 	 * 
 	 * @param element	the integer to be added to the SimpleList
 	 */
 	public void add(int element) {
-		for (int iterate = MAXSIZE - 1; iterate > 0; iterate--)
+		if (count == list.length) {
+			int[] tempList = new int[list.length];
+			
+			for (int iterate = 0; iterate < list.length; iterate++)
+				tempList[iterate] = list[iterate];
+			
+			list = new int[(int) (list.length * INCREASE_SIZE)];
+			
+			for (int iterate = 0; iterate < tempList.length; iterate++)
+				list[iterate] = tempList[iterate];
+		}
+		
+		for (int iterate = list.length - 1; iterate > 0; iterate--)
 			list[iterate] = list[iterate - 1];
 		
 		list[0] = element;
-		count = count == MAXSIZE ? count : ++count;
+		++count;
 	}
 	
 	/**
 	 * Remove an element from the index the search method returns and then
-	 * shifting all elements to the right of it to the left.
+	 * shifting all elements to the right of it to the left. After the element
+	 * is removed, if more than 25% of the Simple List is unused then the list
+	 * is copied over to a new list that is 25% smaller in size.
 	 * 
 	 * @param element	the integer to be removed from the SimpleList
 	 */
@@ -71,13 +89,25 @@ public class SimpleList {
 		int position = search(element);
 		if (position == -1) return;
 		
-		while (position < (MAXSIZE - 1)) {
+		while (position < (count - 1)) {
 			list[position] = list[position + 1];
 			position++;
 		}
-		
-		list[MAXSIZE - 1] = 0;
+
 		count--;
+		float sizeOfEmpty = 1 - ((float) count / list.length);
+		
+		if (list.length != SMALLEST_SIZE && sizeOfEmpty > EMPTY_THRESHOLD) {
+			int[] tempList = new int[list.length];
+			
+			for (int iterate = 0; iterate < list.length; iterate++)
+				tempList[iterate] = list[iterate];
+			
+			list = new int[(int) (list.length * DECREASE_SIZE)];
+			
+			for (int iterate = 0; iterate < list.length; iterate++)
+				list[iterate] = tempList[iterate];
+		}
 	}
 	
 	/**
@@ -97,10 +127,10 @@ public class SimpleList {
 	public String toString() {
 		String listString = "";
 		
-		for (int iterate = 0; iterate < MAXSIZE; iterate++) {
+		for (int iterate = 0; iterate < count; iterate++) {
 			listString = listString.concat(Integer.toString(list[iterate]));
 			
-			if (iterate != (MAXSIZE - 1))
+			if (iterate != (count - 1))
 				listString = listString.concat(" ");
 		}
 		return listString;
@@ -115,7 +145,7 @@ public class SimpleList {
 	public int search(int element) {
 		int index = -1;
 		
-		for (int iterate = 0; iterate < MAXSIZE; iterate++)
+		for (int iterate = 0; iterate < count; iterate++)
 			if (list[iterate] == element)
 				index = iterate;
 		
